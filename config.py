930c249ce45eb73cf59c8ad5a1379153d23595cf
= <=>
2025-06-06 07:00:09 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,9 @@
+DATASET_REPO = "FrancophonIA/EU_scientific_corpus"
+DATA_DIR = "EU_science_data/"
+SOURCE_LANG = "English"
+TARGET_LANG = "Estonian"
+
+LOCAL_DIR = "./EU_scientific_corpus"
+CACHE_DIR = "./model_cache"
+MODEL_NAME = "google/gemma-3-4b-pt"
+OUTPUT_DIR = "gemma3 4b-finetuning"
